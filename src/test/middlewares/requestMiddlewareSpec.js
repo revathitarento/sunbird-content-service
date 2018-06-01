2dfc091de0c00d6d6f19031cd30622f6365a776c
revathi <revathipp@gmail.com>
2018-06-01 22:03:21 +0530

    Issue #SB-2658 fix: Test cases

---
+++ src/test/middlewares/requestMiddlewareSpec.js
@@ -0,0 +1,46 @@
+var configService = require('../../service/configService');
+var req = require('request');
+
+mockSuccesResponse = {
+    id: "api.v1.search",
+    params: { resmsgid: "b131b010-6576-11e8-acde-61b03635ae59", msgid: "b10f8210-6576-11e8-a86b-a1a09e4aff25" },
+    responseCode: "OK",
+    result: [{
+        appId: "qa.ekstep.in",
+        channel: "in.ekstep"
+    }, {
+        appId: "qa.ekstep.in",
+        channel: "in.ekstep"
+    }]
+};
+
+mockErrResponse = {
+    id: "api.v1.search",
+    params: { resmsgid: "b131b010-6576-11e8-acde-61b03635ae59", msgid: "b10f8210-6576-11e8-a86b-a1a09e4aff25" },
+    responseCode: "OK",
+    statusCode: "500",
+    error: "CLIENT_ERROR",
+    result: ''
+};
+
+describe("filter of channels", function () {
+    it('check getchannelsgetChannelSearchStringearchstring function', function () {
+        spyOn(configService, 'getChannelSearchString').and.callFake(mockErrResponse);
+        // setTimeout(() => {
+            configService.getChannelSearchString(req, mockErrResponse);
+        // }, 100);
+      
+        // expect(configService.getChannelSearchString).toHaveBeenCalled();
+        expect(mockErrResponse.statusCode).toBe(500)
+        expect(mockErrResponse.error).toBeDefined()
+    });
+});
+
+// describe("Testing for error response", function() {
+// it('get Channel to be called with error', function() { 
+//     spyOn(configService, 'getChannelSearchString').and.throwError();
+//     configService.getChannelSearchString();
+//     expect(configService.getChannelSearchString).toHaveBeenCalled();
+// } );
+
+// });
---
+++ src/test/service/configServiceSpec.js
@@ -0,0 +1,69 @@
+var configService = require("../../service/configService.js");
+var whiteListedChannelList = ['in.ekstep', 'abcd'];
+var blackListedChannelList = ['xyz', 'zxy'];
+var allowedChannels = whiteListedChannelList;
+var blackListedChannels = blackListedChannelList;
+
+var searchString = {};
+var req = require('request');
+
+var mockAllowed = whiteListedChannelList
+var mockErrResponse = true;
+
+
+describe('getChannelSearchString', function () {
+    process.env.sunbird_content_service_whitelisted_channels_new = ['in.ekstep'];
+    console.log("whitelisted", process.env.sunbird_content_service_whitelisted_channels_new)
+    process.env.sunbird_content_service_blacklisted_channels_new = ['sun.channel']
+
+    describe('generateChannelSearchString for allowed channels', function () {
+
+        it('allowed channels exists', function () {
+            expect(allowedChannels.length).toEqual(2);
+            expect(whiteListedChannelList).toBeDefined();
+        });
+
+        it('check the return value', function () {
+            function getChannelSearchString(req, callback) {
+                setTimeout(function (callback) {
+                    callback();
+                }, 1000);
+            }
+        });
+
+        it('if allowedchannels is greater than 0', function () {
+            searchString = allowedChannels;
+            expect(allowedChannels).toBe(whiteListedChannelList);
+            expect(allowedChannels.length).toBeGreaterThan(0);
+            expect(searchString).toEqual(allowedChannels);
+        });
+
+        if (allowedChannels) {
+            it("for allowed Channels", function () {
+                searchString = allowedChannels;
+                expect(allowedChannels).toEqual(searchString);
+            });
+        } else {
+            it("for blacklisted channels", function () {
+                searchString = { 'ne': blackListedChannels };
+                expect(blackListedChannels).toEqual(searchString);
+
+            });
+        }
+    });
+    describe('generate Channel String for blacklisted channels', function () {
+
+        it('blackListed channels exists', function () {
+            expect(blackListedChannels.length).toEqual(2);
+            expect(blackListedChannels).toBeDefined();
+        });
+
+        it('check the return value', function () {
+            whiteListedChannelList = ['fsdfsd', 'fdsfds'];
+            blackListedChannelList = ['fdsf', 'fsdfdsfdasfsadf']
+            spyOn(getChannelSearchString, 'generateChannelSearchString')
+            getChannelSearchString.generateChannelSearchString(whiteListedChannelList, blackListedChannelList)
+            expect(getChannelSearchString.getChannelSearchString).toHaveBeenCalled();
+        });
+    });
+});
